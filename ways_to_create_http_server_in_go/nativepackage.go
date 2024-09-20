11cecd3e0548bbb5095a91751a0367a6fba51b8c
A.Tanoumandian <tanoomandian204@gmail.com>
2024-09-20 07:23:18 +0330

    feat: native package sample added

---
+++ ways_to_create_http_server_in_go/nativepackage.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type baseResponse struct {
+	Message string                 `json:"message"`
+	Data    map[string]interface{} `json:"data"`
+}
+
+func handle404Request(w http.ResponseWriter) {
+	// set `Content-Type` header
+	w.Header().Add("Content-Type", "application/json")
+
+	// set response status code
+	w.WriteHeader(http.StatusNotFound)
+
+	// encode response struct to json
+	res, _ := json.Marshal(
+		baseResponse{
+			Message: "not found",
+			Data: map[string]interface{}{
+				"detail": "requested resource not found",
+			},
+		},
+	)
+
+	fmt.Fprint(w, string(res))
+
+	log.Default().Println("request resource not found")
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet || r.RequestURI != "/" {
+		handle404Request(w)
+		return
+	}
+
+	// encode response struct to json
+	res, _ := json.Marshal(
+		baseResponse{
+			Message: "http server is running with native package",
+			Data:    map[string]interface{}{"created_by": "ARTM2000"},
+		},
+	)
+
+	// set `Content-Type` header
+	w.Header().Add("Content-Type", "application/json")
+
+	// set response status code
+	w.WriteHeader(http.StatusOK)
+
+	// write the response
+	fmt.Fprint(w, string(res))
+}
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	log.Default().Println("server listen on port 3000!")
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		log.Fatalf("server failed to start: %s", err.Error())
+	}
+}
